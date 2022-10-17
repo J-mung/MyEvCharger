e77ee2157349a75410fe3f013ae3d3633918f852
TAEGYU JANG <xorb2298@gmail.com>
2022-10-17 21:19:37 +0900

    Init 종속 항목

---
+++ app/src/main/java/com/example/myevcharger/di/NetworkModule.kt
@@ -0,0 +1,19 @@
+package com.example.myevcharger.di
+
+import com.example.myevcharger.utils.Constants.Companion.BASE_URL
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object NetworkModule {
+    private var instance: Retrofit? = null
+
+    open fun getInstance(): Retrofit {
+        if (instance == null) {
+            instance = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+        return instance!!
+    }
+}
