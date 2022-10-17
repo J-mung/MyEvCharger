10f8dbc35f5bc371847c3cdc055463af77b28a75
TAEGYU JANG <xorb2298@gmail.com>
2022-10-17 21:12:48 +0900

    Add Retrofit instance 및 Edit Api

---
+++ app/src/main/java/com/example/myevcharger/presentation/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myevcharger
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URL
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
