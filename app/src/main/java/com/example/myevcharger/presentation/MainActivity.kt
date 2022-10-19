cc516a6b2397f02e5c996cfd10bfe84baf78c796
TAEGYU JANG <xorb2298@gmail.com>
2022-10-19 20:57:21 +0900

    Test baseActivity/ViewModel 적용

--- app/src/main/java/com/example/myevcharger/presentation/MainActivity.kt
+++ app/src/main/java/com/example/myevcharger/presentation/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.example.myevcharger.presentation
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.ViewDataBinding
 import com.example.myevcharger.R
+import com.example.myevcharger.databinding.ActivityMainBinding
+import com.example.myevcharger.domain.model.EvChargerResponse
+import com.example.myevcharger.domain.repository.local.LocalEvChargerRepository
+import com.example.myevcharger.presentation.base.BaseActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +17,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        /*
+        var tv: TextView = findViewById(R.id.tv)
+        tv.setText("Hello world!")
+        */
+    }
+
+    fun getChargerList() {
+        val repository: LocalEvChargerRepository
+        repository.getAllCharger().toString()
     }
 }
