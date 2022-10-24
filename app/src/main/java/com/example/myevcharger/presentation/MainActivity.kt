60ee2629e44fbd38103a3b233f7df9b2ef7cf6f6
TAEGYU JANG <xorb2298@gmail.com>
2022-10-25 02:03:42 +0900

    Fix baseActivity ActivityMainBinding type argument error 해결

--- app/src/main/java/com/example/myevcharger/presentation/MainActivity.kt
+++ app/src/main/java/com/example/myevcharger/presentation/MainActivity.kt
@@ -3,16 +3,13 @@ package com.example.myevcharger.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myevcharger.R
+import com.example.myevcharger.databinding.ActivityMainBinding
+import com.example.myevcharger.presentation.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity() : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-        /*
-        var tv: TextView = findViewById(R.id.tv)
-        tv.setText("Hello world!")
-        */
     }
 }
--- app/src/main/java/com/example/myevcharger/presentation/base/BaseActivity.kt
+++ app/src/main/java/com/example/myevcharger/presentation/base/BaseActivity.kt
@@ -2,24 +2,26 @@ package com.example.myevcharger.presentation.base
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
-abstract class BaseActivity<T: ViewDataBinding, R: BaseViewModel>: AppCompatActivity() {
-    lateinit var viewDataBinding: T
-
-    abstract val TAG: String
-    abstract val layoutResID: Int
-    abstract val viewModel: R
+abstract class BaseActivity<T: ViewDataBinding>(
+    @LayoutRes private val layoutResId: Int
+): AppCompatActivity() {
+    private var _binding: T? = null
+    private val binding: T
+        get() = requireNotNull(_binding)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        _binding = DataBindingUtil.setContentView(this, layoutResId)
+        binding.lifecycleOwner = this
+    }
 
-        // 초기화된 layoutResId로 databinding 객체 생성
-        viewDataBinding = DataBindingUtil.setContentView(this, layoutResID)
-
-        // live data를 사용하기 위해 set
-        viewDataBinding.lifecycleOwner = this@BaseActivity
+    override fun onDestroy() {
+        binding.unbind()
+        super.onDestroy()
     }
 }
